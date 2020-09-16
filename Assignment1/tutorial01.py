6fe4e84366ac5896628f563d27ea02dee0628ee3
pranshukapri <pranshukapri@gmail.com>
2020-09-16 20:02:03 +0530

    This is my first git commit

---
+++ Assignment1/tutorial01.py
@@ -0,0 +1,20 @@
+# Function to add two numbers 
+def add(num1, num2): 
+	addition = num1 + num2
+	return addition
+
+# Function to subtract two numbers 
+def subtract(num1, num2): 
+	subtraction = num1 - num2
+	return subtraction
+
+# Function to multiply two numbers 
+def multiply(num1, num2): 
+	multiplication = num1 * num2
+	return multiplication
+
+# Function to divide two numbers 
+def divide(num1, num2): 
+	division = num1 / num2 
+	return division
+	
