67ce15b3ac9c20033a1af9b4ba68476e4477507e
pranshukapri <pranshukapri@gmail.com>
2020-09-24 12:41:56 +0530

    Added the function for nse in tutorial02

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -1,9 +1,17 @@
 import math
 # All decimal 3 places
 
+def check(listx):
+    for i in listx:
+        if isinstance(i, str):
+            return 1
+    return 0
+
 # Function to compute mean
 def mean(first_list):
     # mean Logic 
+    if check(first_list):
+        return 0
     count = 0
     for i in first_list:
         count = count + 1
@@ -13,25 +21,36 @@ def mean(first_list):
 
 # Function to compute median. You cant use Python functions
 def median(first_list):
+    if check(first_list):
+        return 0
     # median Logic
+    median_value = 0
     return median_value
 
 
 # Function to compute Standard deviation. You cant use Python functions
 def standard_deviation(first_list):
+    if check(first_list):
+        return 0
     # Standard deviation Logic
+    standard_deviation_value = 0
     return standard_deviation_value
 
 
 # Function to compute variance. You cant use Python functions
 def variance(first_list):
+    if check(first_list):
+        return 0
     # variance Logic
+    variance_value = 0
     return variance_value
 
 
 # Function to compute RMSE. You cant use Python functions
 def rmse(first_list, second_list):
     # RMSE Logic
+    if check(first_list) | check(second_list):
+        return 0
     rmse_value = math.sqrt(mse(first_list, second_list))
     return round(rmse_value,3)
 
@@ -39,6 +58,8 @@ def rmse(first_list, second_list):
 # Function to compute mse. You cant use Python functions
 def mse(first_list, second_list):
     # mse Logic
+    if check(first_list) | check(second_list):
+        return 0
     count = 0
     mse_value = 0
     for i in first_list:
@@ -51,39 +72,65 @@ def mse(first_list, second_list):
 # Function to compute mae. You cant use Python functions
 def mae(first_list, second_list):
     # mae Logic
+    if check(first_list) | check(second_list):
+        return 0
+    mae_value = 0
     return mae_value
 
 
 # Function to compute NSE. You cant use Python functions
 def nse(first_list, second_list):
     # nse Logic
-    return nse_value
+    if check(first_list) | check(second_list):
+        return 0
+    mean1 = mean(first_list)
+    den = 0
+    count = 0
+    for i in first_list:
+        den = den + (i - mean1)*(i - mean1)
+        count = count + 1
+    num = count * mse(first_list, second_list)
+    nse_value = 1 - (num/den)
+    return round(nse_value,3)
 
 
 # Function to compute Pearson correlation coefficient. You cant use Python functions
 def pcc(first_list, second_list):
     # nse Logic
+    if check(first_list) | check(second_list):
+        return 0
+    pcc_value = 0
     return pcc_value
 
 
 # Function to compute Skewness. You cant use Python functions
 def skewness(first_list):
+    if check(first_list):
+        return 0
     # Skewness Logic
+    skewness_value = 0
     return skewness_value
     
 def sorting(first_list):
+    if check(first_list):
+        return 0
     # Sorting Logic
     return sorted_list
 
 
 # Function to compute Kurtosis. You cant use Python functions
 def kurtosis(first_list):
+    if check(first_list):
+        return 0
     # Kurtosis Logic
+    kurtosis_value = 0
     return kurtosis_value
 
 
 # Function to compute sum. You cant use Python functions
 def summation(first_list):
+    if check(first_list):
+        return 0
     # sum Logic
     summation_value = 0
     for i in first_list:
