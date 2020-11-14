5023517652eafe19c6f39738a5265cc76bdc47e3
pranshukapri <pranshukapri@gmail.com>
2020-11-14 15:58:18 +0530

    Updated the final working code for all functions

--- Assignment/Assignment 5 Web Series Renamer/tutorial05.py
+++ Assignment/Assignment 5 Web Series Renamer/tutorial05.py
@@ -13,18 +13,31 @@ def rename_FIR(folder_name):
         
         ext = '.' + re.search(r'(.{3})$', name).group(1)
         
+        e_name = re.search(r'\d\s-\s(.*?)-', name)
+        
+        if not e_name:
+            e_name = ''
+        else:
+            e_name = ' - ' + e_name.group(1)
+        
         while 1:
             if len(e_no) >= e_padding:
                 break
             else:
                 e_no = '0' + e_no
         
-        new_name = folder_name + ' - ' + 'Episode' + ' ' + e_no
+        new_name = folder_name + ' - ' + 'Episode' + ' ' + e_no + e_name
         
         try:
             os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ext)
         except WindowsError:
-            os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + '(1)' + ext)
+            I = 1
+            while 1:
+                if os.path.isfile("./Subtitles/" + folder_name + "/" + new_name + ' (' + str(I) + ')' + ext):
+                    I = I + 1
+                else:
+                    os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ' (' + str(I) + ')' + ext)
+                    break
 
 def rename_Game_of_Thrones(folder_name):
     # rename Logic 
@@ -61,7 +74,13 @@ def rename_Game_of_Thrones(folder_name):
         try:
             os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ext)
         except WindowsError:
-            os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + '(1)' + ext)
+            I = 1
+            while 1:
+                if os.path.isfile("./Subtitles/" + folder_name + "/" + new_name + ' (' + str(I) + ')' + ext):
+                    I = I + 1
+                else:
+                    os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ' (' + str(I) + ')' + ext)
+                    break
 
 def rename_Sherlock(folder_name):
     # rename Logic 
@@ -92,7 +111,13 @@ def rename_Sherlock(folder_name):
         try:
             os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ext)
         except WindowsError:
-            os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + '(1)' + ext)
+            I = 1
+            while 1:
+                if os.path.isfile("./Subtitles/" + folder_name + "/" + new_name + ' (' + str(I) + ')' + ext):
+                    I = I + 1
+                else:
+                    os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ' (' + str(I) + ')' + ext)
+                    break
 
 def rename_Suits(folder_name):
     # rename Logic 
@@ -129,7 +154,13 @@ def rename_Suits(folder_name):
         try:
             os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ext)
         except WindowsError:
-            os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + '(1)' + ext)
+            I = 1
+            while 1:
+                if os.path.isfile("./Subtitles/" + folder_name + "/" + new_name + ' (' + str(I) + ')' + ext):
+                    I = I + 1
+                else:
+                    os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ' (' + str(I) + ')' + ext)
+                    break
 
 def rename_How_I_Met_Your_Mother(folder_name):
     # rename Logic 
@@ -137,25 +168,72 @@ def rename_How_I_Met_Your_Mother(folder_name):
     e_padding = int(input("Enter the Episode Number Padding:"))
     list = os.listdir("./Subtitles/" + folder_name)
     for name in list:
-        print(name)
-    
-print("Select the Series to Rename:")
-print("1. FIR")
-print("2. Game of Thrones")
-print("3. Sherlock")
-print("4. Suits")
-print("5. How I Met Your Mother")
-i = int(input())
+        
+        e_no = re.search(r'\s(\d+)x(.*?)\s', name).group(2)
+        s_no = re.search(r'\s(\d+)x(.*?)\s', name).group(1)
+        ext = '.' + re.search(r'(.{3})$', name).group(1)
+        
+        e_name = re.search(r'\s(\d+)x(.*?)\s-\s(.*?)\.', name)
+        
+        if not e_name:
+            e_name = ''
+        else:
+            e_name = ' - ' + e_name.group(3)
+        
+        while 1:
+            if len(e_no) >= e_padding:
+                break
+            else:
+                e_no = '0' + e_no
+        
+        while 1:
+            if len(s_no) >= s_padding:
+                break
+            else:
+                s_no = '0' + s_no
+        
+        new_name = folder_name + ' - Season ' + s_no + ' Episode ' + e_no + e_name
+        
+        try:
+            os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ext)
+        except WindowsError:
+            I = 1
+            while 1:
+                if os.path.isfile("./Subtitles/" + folder_name + "/" + new_name + ' (' + str(I) + ')' + ext):
+                    I = I + 1
+                else:
+                    os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ' (' + str(I) + ')' + ext)
+                    break
 
-if i == 1:
-    rename_FIR("FIR")
-elif i == 2:
-    rename_Game_of_Thrones("Game of Thrones")
-elif i == 3:
-    rename_Sherlock("Sherlock")
-elif i == 4:
-    rename_Suits("Suits")
-elif i == 5:
-    rename_How_I_Met_Your_Mother("How I Met Your Mother")
-else:
-    print("Wrong Input")
+while 1:    
+    print("Select the Series to Rename:")
+    print("1. FIR")
+    print("2. Game of Thrones")
+    print("3. Sherlock")
+    print("4. Suits")
+    print("5. How I Met Your Mother")
+    print("6. Exit...")
+    i = int(input())
+
+    if i == 1:
+        rename_FIR("FIR")
+        print("Done!")
+    elif i == 2:
+        rename_Game_of_Thrones("Game of Thrones")
+        print("Done!")
+    elif i == 3:
+        rename_Sherlock("Sherlock")
+        print("Done!")
+    elif i == 4:
+        rename_Suits("Suits")
+        print("Done!")
+    elif i == 5:
+        rename_How_I_Met_Your_Mother("How I Met Your Mother")
+        print("Done!")
+    elif i == 6:
+        print("Thank You!")
+        print("")
+        break
+    else:
+        print("Wrong Input")
+    print("")
