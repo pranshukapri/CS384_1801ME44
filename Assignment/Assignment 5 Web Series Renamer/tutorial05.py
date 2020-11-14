888cdb823825a15ab154c3670a444edf2e41dd83
pranshukapri <pranshukapri@gmail.com>
2020-11-14 15:21:01 +0530

    Updated the code for Renaming Sherlock series

--- Assignment/Assignment 5 Web Series Renamer/tutorial05.py
+++ Assignment/Assignment 5 Web Series Renamer/tutorial05.py
@@ -69,7 +69,30 @@ def rename_Sherlock(folder_name):
     e_padding = int(input("Enter the Episode Number Padding:"))
     list = os.listdir("./Subtitles/" + folder_name)
     for name in list:
-        print(name)
+        
+        s_no = re.search(r'\.S(\d+)', name).group(1)
+        e_no = re.search(r'E(\d+)\.', name).group(1)
+        ext = '.' + re.search(r'(.{3})$', name).group(1)
+        
+        while 1:
+            if len(e_no) >= e_padding:
+                break
+            else:
+                e_no = '0' + e_no
+        
+        while 1:
+            if len(s_no) >= s_padding:
+                break
+            else:
+                s_no = '0' + s_no
+        
+        new_name = folder_name + ' - Season ' + s_no + ' Episode ' + e_no
+        
+        
+        try:
+            os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + ext)
+        except WindowsError:
+            os.rename("./Subtitles/" + folder_name + "/" + name, "./Subtitles/" + folder_name + "/" + new_name + '(1)' + ext)
 
 def rename_Suits(folder_name):
     # rename Logic 
